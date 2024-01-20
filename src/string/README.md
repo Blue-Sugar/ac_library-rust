27134579ff31b5e6b90f991e16cc77606522a8dc
Blue_Sugar <aoicchi0620@gmail.com>
2024-01-20 16:01:09 +0900

    add: string_next

--- src/string/README.md
+++ src/string/README.md
@@ -4,3 +4,8 @@
 ### `fn is_palindrome(s: Vec<T>) -> bool`
 `s`がpalindromeになっているかどうかを返す。
 $O(|s|)$
+
+### `fn next(s: &Vec<u8>) -> Vec<Vec<usize>>`
+`res[char][i]`は文字種`char`のインデックス`i`以降で出てくるインデックスを表す。ここで、`i`以降は`i`を含む。
+`i = s.len()`ととれ、このときは確定で`INF`を返す。
+$O(|s| \times |set(char)|)$
--- src/string/mod.rs
+++ src/string/mod.rs
@@ -4,3 +4,19 @@ fn is_palindrome<T: Eq + Clone>(s: Vec<T>) -> bool {
     t.reverse();
     s == t
 }
+
+const INF: usize = std::usize::MAX / 2;
+#[allow(unused)]
+fn next(s: &Vec<u8>, from: u8, to: u8) -> Vec<Vec<usize>> {
+    let mut res = vec![vec![INF; s.len() + 1]; (to - from + 1) as usize];
+    for c in from..=to {
+        let mut last = INF;
+        for i in (0..s.len()).rev() {
+            if s[i] == c {
+                last = i;
+            }
+            res[(c - from) as usize][i] = last;
+        }
+    }
+    res
+}
