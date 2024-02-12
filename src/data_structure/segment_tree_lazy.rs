c46fe9f1df96bcbd20af4487371a0dc0cd44da0b
Blue_Sugar <aoicchi0620@gmail.com>
2024-02-12 19:40:39 +0900

    add: lazy segment tree of range query

---
+++ src/data_structure/segment_tree_lazy.rs
@@ -0,0 +1,145 @@
+#[allow(unused)]
+pub struct SegmentTreeLazy<M, A> {
+    n: usize,
+    size: usize,
+    data: Vec<(M, A)>,    
+}
+
+#[allow(unused)]
+impl<M, A> SegmentTreeLazy<M, A>
+where 
+    M: Monoid,
+    A: Monoid + Act<M>,
+{
+    pub fn new(n: usize) -> Self {
+        let size = n.next_power_of_two();
+        SegmentTreeLazy {
+            n: n,
+            data: vec![(M::e(), A::e()); 2 * size],
+            size: size,
+        }
+    }
+    
+    pub fn build(a: &Vec<M>) -> Self {
+        let size = a.len().next_power_of_two();
+        let mut data = vec![(M::e(), A::e()); 2 * size];
+        for (data, a) in data[size..].iter_mut().zip(a.iter()) {
+            data.0 = *a;
+        }
+        let mut res = SegmentTreeLazy {
+            n: a.len(),
+            size: size,
+            data: data,
+        };
+        for i in (1..size).rev() {
+            res.save_at(i);
+        }
+        res
+    }
+    
+    fn apply_at(&mut self, x: usize, f: &A) {
+        let tmp = &mut self.data[x];
+        *tmp = (f.act(&tmp.0), tmp.1.op(f));
+    }
+
+    fn propagate_at(&mut self, x: usize) {
+        let f = std::mem::replace(&mut self.data[x].1, A::e());
+        self.apply_at(2 * x, &f);
+        self.apply_at(2 * x + 1, &f);
+    }
+
+    fn save_at(&mut self, x: usize) {
+        self.data[x].0 = self.data[2 * x].0.op(&self.data[2 * x + 1].0);
+    }
+
+    fn propagate(&mut self, l: usize, r: usize) {
+        let l = l + self.size;
+        let r = r + self.size;
+        for i in (1..=self.size.trailing_zeros() as usize).rev() {
+            if (l >> i) << i != l {
+                self.propagate_at(l >> i);
+            }
+            if (r >> i) << i != r {
+                self.propagate_at((r - 1) >> i);
+            }
+        }
+    }
+
+    fn save(&mut self, l: usize, r: usize) {
+        let l = l + self.size;
+        let r = r + self.size;
+        for i in 1..=self.size.trailing_zeros() as usize {
+            if (l >> i) << i != l {
+                self.save_at(l >> i);
+            }
+            if (r >> i) << i != r {
+                self.save_at((r - 1) >> i);
+            }
+        }
+    }
+
+    pub fn action(&mut self, l: usize, r: usize, f: A) {
+        if l == r {
+            return;
+        }
+        self.propagate(l, r);
+        let mut x = l + self.size;
+        let mut y = r + self.size;
+        while x < y {
+            if x & 1 == 1 {
+                self.apply_at(x, &f);
+                x += 1;
+            }
+            if y & 1 == 1 {
+                y -= 1;
+                self.apply_at(y, &f);
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        self.save(l, r);
+    }
+
+    pub fn prod(&mut self, l: usize, r: usize) -> M {
+        if l == r {
+            return M::e();
+        }
+        self.propagate(l, r);
+        let mut x = l + self.size;
+        let mut y = r + self.size;
+        let mut p = M::e();
+        let mut q = M::e();
+        while x < y {
+            if x & 1 == 1 {
+                p = p.op(&self.data[x].0);
+                x += 1;
+            }
+            if y & 1 == 1 {
+                y -= 1;
+                q = self.data[y].0.op(&q);
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        p.op(&q)
+    }
+
+    pub fn set_at(&mut self, x: usize, v: M) {
+        let y = x + self.size;
+        for i in (1..=self.size.trailing_zeros() as usize).rev() {
+            self.propagate_at(y >> i);
+        }
+        self.data[y].0 = v;
+        for i in 1..=self.size.trailing_zeros() as usize {
+            self.save_at(y >> i);
+        }
+    }
+
+    pub fn get_at(&mut self, x: usize) -> M {
+        let y = x + self.size;
+        for i in (1..=self.size.trailing_zeros() as usize).rev() {
+            self.propagate_at(y >> i);
+        }
+        self.data[y].0
+    }
+}
--- src/math/algebra/monoid.rs
+++ src/math/algebra/monoid.rs
@@ -3,3 +3,85 @@ pub trait Monoid: Clone + Copy {
     fn e() -> Self;
     fn op(&self, rhs: &Self) -> Self;
 }
+#[allow(unused)]
+pub trait Act<S>: Clone + Copy {
+    fn act(&self, x: &S) -> S;
+}
+
+
+
+#[allow(unused)]
+impl Monoid for (isize, isize) {
+    fn e() -> Self {
+        (0, 1)
+    }
+    fn op(&self,rhs: &Self) -> Self {
+        (self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+#[derive(Clone, Copy)]
+#[allow(unused)]
+enum Action {
+    UnitAction,
+    UpdateAction(isize),
+    AddAction(isize),
+    MulAction(isize),
+    AffineAction(isize, isize),
+}
+impl Monoid for Action {
+    fn e() -> Self {
+        Action::UnitAction
+    }
+    fn op(&self,rhs: &Self) -> Self {
+        match self {
+            Action::UnitAction => *rhs,
+            Action::UpdateAction(u) => {
+                match rhs {
+                    Action::UnitAction => *self,
+                    Action::UpdateAction(v) => Action::UpdateAction(*v),
+                    Action::AddAction(b) => Action::UpdateAction(u + b),
+                    Action::MulAction(b) => Action::UpdateAction(u * b),
+                    Action::AffineAction(b, d) => Action::UpdateAction(u * b + d),
+                }
+            }
+            Action::AddAction(a) => {
+                match rhs {
+                    Action::UnitAction => *self,
+                    Action::UpdateAction(v) => Action::UpdateAction(*v),
+                    Action::AddAction(b) => Action::AddAction(a + b),
+                    Action::MulAction(b) => Action::AffineAction(*b, a * b),
+                    Action::AffineAction(b, d) => Action::AffineAction(*b, a * b + d),
+                }
+            }
+            Action::MulAction(a) => {
+                match rhs {
+                    Action::UnitAction => *self,
+                    Action::UpdateAction(v) => Action::UpdateAction(*v),
+                    Action::AddAction(b) => Action::AffineAction(*a, *b),
+                    Action::MulAction(b) => Action::MulAction(a * b),
+                    Action::AffineAction(b, d) => Action::AffineAction(a * b, *d),
+                }
+            }
+            Action::AffineAction(a, c) => {
+                match rhs {
+                    Action::UnitAction => *self,
+                    Action::UpdateAction(v) => Action::UpdateAction(*v),
+                    Action::AddAction(b) => Action::AffineAction(*a, c + b),
+                    Action::MulAction(b) => Action::AffineAction(a * b, b * c),
+                    Action::AffineAction(b, d) => Action::AffineAction(a * b, c * b + d),
+                }
+            }
+        }
+    }
+}
+impl Act<(isize, isize)> for Action {
+    fn act(&self, x: &(isize, isize)) -> (isize, isize) {
+        match self {
+            Action::UnitAction => *x,
+            Action::UpdateAction(u) => (*u, x.1),
+            Action::AddAction(a) => (x.0 + a * x.1, x.1),
+            Action::MulAction(a) => (a * x.0, x.1),
+            Action::AffineAction(a, c) => (a * x.0 + c * x.1, x.1),
+        }
+    }
+}
